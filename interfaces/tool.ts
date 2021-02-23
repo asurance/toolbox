a6624a11c1cccb605d108b9e14e2d99ab630f453
asurance <784672616@qq.com>
2021-02-23 18:46:12 +0800

    tool配置导出为json

--- interfaces/tool.ts
+++ interfaces/tool.ts
@@ -3,18 +3,5 @@ export type Tool = {
     name: string;
     url: string;
     description: string;
-    tag: Tag;
+    tag: string;
 }
-
-export type Tag =
-    '3d资源' |
-    '独立游戏' |
-    '音频' |
-    'WebGL' |
-    '2d资源' |
-    '数学' |
-    'js库' |
-    '字体' |
-    'Rust' |
-    '前端' |
-    'chrome插件'
