d170a5bacd8ddf547f646d26b6cecad391641944
asurance <784672616@qq.com>
2021-09-15 09:00:50 +0800

    修改配置

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pwa: {
+    msTileColor: "#b7e4c7",
+    manifestOptions: {
+      background_color: "#b7e4c7",
+    },
+    name: "Asurance的工具箱",
+    themeColor: "#b7e4c7",
+  },
+};
